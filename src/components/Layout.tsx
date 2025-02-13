5078232afd39cbae6d6de9eab511704557c95abc
Hai M Nguyen <vnjp22@gmail.com>
2025-02-13 09:02:20 -0800

    added link to youtube channel

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,6 +15,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             </main>
             <footer className={styles.footer}>
                 <p>&copy; 2025 Bestopia. All rights reserved.</p>
+                <p>
+                    <a href="https://www.youtube.com/@Bestopia_net" target="_blank" rel="noopener noreferrer">
+                        <img src="/img/youtube_logo.png" alt="Bestopia - YouTube" className={styles.youtubeLogo} />
+                    </a>
+                </p>
             </footer>
         </div>
     );
