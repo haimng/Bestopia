1404c86ebb917d21da10c92b73f8a8e2a499276b
Hai M Nguyen <vnjp22@gmail.com>
2025-02-16 13:39:00 -0800

    Use Crawlbase api to crawl amazon product

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -37,34 +37,10 @@ async function crawlProduct(product: any) {
     const { name } = product;
     const params = new URLSearchParams({ k: name });
     const url = `https://www.amazon.com/s?${params.toString()}`;
+    const crawlerApiUrl = `https://api.crawlbase.com/?token=ALhEGfr3FUD0gwwN-9m3QQ&url=${encodeURIComponent(url)}`;
 
     try {
-        const response = await fetch(url, {
-            headers: {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "accept-language": "en-US,en;q=0.9",
-                "cache-control": "no-cache",
-                "device-memory": "8",
-                "downlink": "10",
-                "dpr": "1",
-                "ect": "4g",
-                "pragma": "no-cache",
-                "priority": "u=0, i",
-                "rtt": "100",
-                "sec-ch-device-memory": "8",
-                "sec-ch-dpr": "1",
-                "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "\"macOS\"",
-                "sec-ch-viewport-width": "1700",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "same-origin",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
-                "viewport-width": "1700"
-            }
-        });
+        const response = await fetch(crawlerApiUrl);
         if (!response.ok) {
             throw new Error(`Failed to fetch data from Amazon: ${response.statusText}`);
         }
