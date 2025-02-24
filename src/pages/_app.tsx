d70d6e845b6046fd9a2137ff5be42503f58d6216
Hai M Nguyen <vnjp22@gmail.com>
2025-02-24 09:36:13 -0800

    Removed Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../styles/globals.css';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
-            <Analytics />
             <SpeedInsights />
         </>
     );
